e6fdb6559d3cbec9d769340f52fd5d6091f07cd9
Mariane Joaquim Melo <marymeelo20@gmail.com>
2025-04-08 13:59:58 -0300

    aa

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import  Profile  from './components/profile';
-import {Gallery, Artist} from './components/profile';
-import Test from './components/Test';
+import 'react-native-gesture-handler';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './Screens/home';
+import Login from './Screens/Login';
+import Feed from './Screens/feed';
 
 
-import { View, StyleSheet } from 'react-native';
+function BottomTabs(){
+  const BottomTab = createBottomTabNavigator();
+  return(
+    <BottomTab.Navigator 
+    screenOptions={{
+      tabBarActiveTintColor:'#6e424b',
+      tabBarActiveBackgroundColor:'#d6b6cb',
+      headerStyle:{backgroundColor: '#d6b6cb'},
+      headerTintColor: '#841584',
+
+    }}>
+      <BottomTab.Screen name='Home' component={Home}
+      options={{
+        tabBarIcon: () => (
+          <MaterialIcons name="home" size={30} color="#FF007F" />
+        ),
+      }} />
+      <BottomTab.Screen name='Feed' component={Feed}
+      options={{
+        tabBarIcon: () => (
+          <MaterialIcons name="feed" size={30} color="#FF007F" />
+        )
+      }} />
+
+    </BottomTab.Navigator>
+  )
+}
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Test/> 
-    </View>
-    
+
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name='Login' component={Login}/>
+      <Stack.Screen options={{headerShown:false}} name='HomeTab' component={BottomTabs}/>
+      </Stack.Navigator>    
+    </NavigationContainer>   
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  }
-})
