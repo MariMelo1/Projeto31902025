bbec4e2f391c316c2bbd2a5f4ceeb5987ce74bc0
Mariane Joaquim Melo <marymeelo20@gmail.com>
2025-05-20 16:20:24 -0300

    read dos produtos do banco

--- Screens/Product.js
+++ Screens/Product.js
@@ -1,21 +1,28 @@
 import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import CardProduct from '../components/cardProducts';
+import {db} from '../controller';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function Product(){
     
-    const [produtos, setProdutos] = useState([
-        {id: 1, nome: 'Macarrão', valor: 2.00, imagem: 'https://s2-receitas.glbimg.com/m2u-cGoinyKpSDYF0BvZJxy8zyA=/696x390/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/R/X/Lj3rwSQpm7BgzSEvJ1Mw/macarrao-simples-como-fazer.jpg'},
-        {id: 2, nome:'Banana', valor: 10.00, imagem: 'https://thumbs.dreamstime.com/b/banna-dos-melhores-frutos-189417825.jpg'},
-        {id: 3, nome: 'Abacaxi', valor: 5.00, imagem: 'https://cloudfront-us-east-1.images.arcpublishing.com/estadao/GEUVV5EDH5FUVEQJ6YYTV3JERA.jpeg' },
-        {id: 4, nome: 'Limão', valor: 8.00 , imagem: 'https://www.saudeemdia.com.br/wp-content/uploads/2022/09/beneficios-do-limao-1.jpg'},
-        {id: 5, nome: 'Kiwi', valor: 75.00 , imagem: 'https://image.tuasaude.com/media/article/jc/dt/emagrecer-com-kiwi_19106.jpg?width=686&height=487'},
-        {id: 6, nome: 'Maracujá', valor: 65.00 , imagem: 'https://carcanplantas.cdn.magazord.com.br/img/2024/08/produto/566/maracuja-doce.png?ims=600x600 '},
-        {id: 7, nome: 'Maça', valor: 80.00, imagem: 'https://image.tuasaude.com/media/article/ix/up/beneficios-da-maca-para-a-saude_40383.jpg?width=686&height=487 ' },
-        {id: 8, nome: 'Chocolate', valor: 95.00 , imagem: 'https://p2.trrsf.com/image/fget/cf/1200/630/middle/images.terra.com/2022/09/06/istock-1313079918-sokmkksb767p.jpg'},
-        {id: 9, nome: 'Pera', valor: 25.00, imagem: 'https://www.svicente.com.br/on/demandware.static/-/Sites-storefront-catalog-sv/default/dwf2d3a042/Produtos/21407-0000000002140-pera%20williams%20kg-flv-1.jpg' },
-        {id: 10, nome: 'Framboesa', valor: 30.00 , imagem: 'https://formasaudavel.com.br/wp-content/uploads/2024/01/Framboesa_fruta.jpg'},
-    ])
+    const [produtos, setProdutos] = useState([])
+
+    useEffect(() => {
+        async function carregarProdutos() {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'produtos'));
+                const lista = [];
+                querySnapshot.forEach((doc) => {
+                    lista.push({id:doc.id, ...doc.data() });
+                });
+                setProdutos(lista);
+            } catch(error){
+                console.log('erro ao buscar o produto', error);
+            }
+        }
+        carregarProdutos();
+    }, []);
 
     return(
         <View style={styles.container}>
@@ -29,7 +36,6 @@ export default function Product(){
              data={produtos}
              renderItem={({item}) => (
                 <CardProduct
-                id ={item.id}
                 nome = {item.nome}
                 valor = {item.valor}
                 imagem = {item.imagem}
